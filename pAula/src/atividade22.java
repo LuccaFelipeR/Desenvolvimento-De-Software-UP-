e1cfd9c83b07ed167a9885ba3910914cfc1fa146
WolffLoney <luccafelipe99@gmail.com>
2026-03-16 21:32:58 -0300

    att 16.03

--- pAula/src/atividade22.java
+++ pAula/src/atividade22.java
@@ -6,13 +6,17 @@ public class atividade22 {
 
         float[] numeros = new float[10];
 
-        float soma = 0;
-
         for (int i = 0; i < 10; i++) {
             System.out.print("Digite o " + (i + 1) + "º número: ");
             numeros[i] = leitor.nextFloat();
-            soma += numeros[i];
         }
+
+        float soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma = soma + numeros[i];
+        }
+        float media = soma / 10;
+
         float maior = numeros[0];
         float menor = numeros[0];
 
@@ -25,17 +29,11 @@ public class atividade22 {
             }
         }
 
-        float media = soma / 10;
-
-        System.out.print("1. Números digitados: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(numeros[i] + " | ");
-        }
-
-        System.out.println("\n2. Soma dos números: " + soma);
-        System.out.println("3. Média aritmética: " + media);
-        System.out.println("4. Maior número: " + maior);
-        System.out.println("5. Menor número: " + menor);
+        System.out.println("\n--- RESULTADOS ---");
+        System.out.println("Soma: " + soma);
+        System.out.println("Média: " + media);
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
 
         leitor.close();
     }
